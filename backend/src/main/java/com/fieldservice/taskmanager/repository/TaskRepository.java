a2cb533757f6a062987ae32297296dc0aba32a2e
jchevireddi <143003083+jchevireddi@users.noreply.github.com>
2025-11-19 13:47:56 -0500

    Delete backend directory

--- backend/src/main/java/com/fieldservice/taskmanager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldservice.taskmanager.repository;
-
-import com.fieldservice.taskmanager.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, String> {
-}
